171a315dd4ef33b012b60104133e7732e40b92fa
Sofía González <sofiagonzaloc@outlook.com>
2023-02-04 13:12:29 -0600

    New database

--- db_config.py
+++ db_config.py
@@ -5,5 +5,5 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@db-remedial.d9z22.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@animalzoo.o0udyjp.mongodb.net/?retryWrites=true&w=majority")
 db = client.zoo
