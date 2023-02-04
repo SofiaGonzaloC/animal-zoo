bc0b49f01d033fdd29c7969f37c8bc75a15a91b8
Sofía González <sofiagonzaloc@outlook.com>
2023-02-04 13:16:38 -0600

    database name change

--- db_config.py
+++ db_config.py
@@ -5,5 +5,5 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@animalzoo.o0udyjp.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_NAME}.o0udyjp.mongodb.net/?retryWrites=true&w=majority")
 db = client.zoo
